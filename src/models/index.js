da7ed66eb4070f100bdd65b4ff6534d5a8892f97
AzhureRaven <abrahamarthurfendy@gmail.com>
2023-05-15 15:36:18 +0700

    Persiapan Hosting

--- src/models/index.js
+++ src/models/index.js
@@ -5,8 +5,10 @@ const path = require('path');
 const {Sequelize, Op} = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/../config/config.js')[env];
+//const db = {};
+const config = require(__dirname + '/../config/config.js');
 const db = {};
 
 let sequelize;
